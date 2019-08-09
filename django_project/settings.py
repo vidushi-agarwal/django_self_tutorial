19b6db2fb4304698addb71ca1aa60d5b91e432a4
vidushi-agarwal <vidushiagarwal1997@gmail.com>
2019-08-09 16:43:56 +0530

    Middleware fixes

--- django_project/settings.py
+++ django_project/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
