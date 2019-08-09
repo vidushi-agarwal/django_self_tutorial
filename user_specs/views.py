959af7986dca0a2688c00a07f017ce544020e537
vidushi-agarwal <vidushiagarwal1997@gmail.com>
2019-08-09 17:21:27 +0530

    Indentation Fixes

--- user_specs/views.py
+++ user_specs/views.py
@@ -18,7 +18,7 @@ def login(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-   return HttpResponse("User is logged in")
+                return HttpResponse("User is logged in")
     if(request.method=="GET"):
         form=Login_form()
     return render(request,"user_specs/login_new.html",{"form":form})
