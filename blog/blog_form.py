7599a0419accd0d826136428e6392fd2233eaae8
vidushi-agarwal <vidushiagarwal1997@gmail.com>
2019-08-13 22:29:26 +0530

    comments

--- blog/blog_form.py
+++ blog/blog_form.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
 from django import forms
 from .models import Blog
-
+// yahaan sb jagah model affect ho rha h islie modelform
+// aur separate model islie nhi bnaya coz dbtable nhi bnani bas table blog mein changes krne h
 class CreateBlog(forms.ModelForm):
     class Meta:
         model=Blog
